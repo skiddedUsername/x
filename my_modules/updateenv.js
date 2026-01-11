260c02266aebba178db7b456cc2b388ca45c2393
skiddedUsername <grizzleot@icloud.com>
2026-01-10 23:29:56 -0500

    Disable updateEnv function
    Disable updateEnv function for Railway environment.

--- my_modules/updateenv.js
+++ my_modules/updateenv.js
@@ -1,17 +1,2 @@
-const envfile = require('envfile')
-const fs = require('fs')
-
-/**
- * 
- * @param {Object} newVariables Object containing key values to insert into .env file
- */
-module.exports = function updateEnv(newVariables){
-    let parsedEnv = envfile.parse(fs.readFileSync('.env', "utf8"))
-
-    for(let key in newVariables){
-        process.env[key] = newVariables[key]
-    }
-
-    Object.assign(parsedEnv, newVariables)
-    fs.writeFileSync('.env', envfile.stringify(parsedEnv))
-}
+// updateenv disabled for Railway — no file writes needed
+module.exports = () => {};
